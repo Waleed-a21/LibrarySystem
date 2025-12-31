e0941a9c7c334d7902bdef6e3f7d38e02d900ff7
waleed <walidlabride3@gmail.com>
2025-12-31 15:30:58 +0100

    Add MemberRepository and tests

---
+++ llllllll/src/main/java/org/example/DatabaseManager.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    private static final String URL = "jdbc:sqlite:library.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
+
+    public static DSLContext getContext() {
+        try {
+            Connection connection = getConnection();
+            return DSL.using(connection, SQLDialect.SQLITE);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to database", e);
+        }
+    }
+}
